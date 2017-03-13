bbfcb6e56d1fff330246b6e7a11c43e312685d83
Suada Haji <suada.haji@andela.com>
2017-03-13 22:32:44 +0300

    Configure splash activity’s theme

--- app/src/main/java/com/example/suadahaji/dijaapplication/SplashActivity.java
+++ app/src/main/java/com/example/suadahaji/dijaapplication/SplashActivity.java
@@ -3,7 +3,7 @@ package com.example.suadahaji.dijaapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
