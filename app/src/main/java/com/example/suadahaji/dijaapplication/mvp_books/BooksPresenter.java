b32e5894526a3faae304d40854ea4c630c1cc514
Suada Haji <suada.haji@andela.com>
2017-03-21 22:17:39 +0300

    Create mvp package for book list

---
+++ app/src/main/java/com/example/suadahaji/dijaapplication/mvp_books/BooksPresenter.java
@@ -0,0 +1,58 @@
+package com.example.suadahaji.dijaapplication.mvp_books;
+
+import com.example.suadahaji.dijaapplication.api.ApiManager;
+import com.example.suadahaji.dijaapplication.models.Book;
+import com.example.suadahaji.dijaapplication.models.BookResponse;
+
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.subscriptions.CompositeSubscription;
+
+/**
+ * Created by suadahaji
+ */
+
+public class BooksPresenter {
+    @Inject
+    ApiManager apiManager;
+
+    private CompositeSubscription compositeSubscription = new CompositeSubscription();
+
+    private BooksContract booksContract;
+
+    public BooksPresenter(ApiManager apiManager, BooksContract contract) {
+        this.apiManager = apiManager;
+        this.booksContract = contract;
+    }
+
+    void fetchBooks() {
+        final Observable<BookResponse> bookResponseObservable = apiManager.getBooks();
+        compositeSubscription.add(bookResponseObservable
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<BookResponse>() {
+                    @Override
+                    public void call(BookResponse bookResponse) {
+                        if (bookResponse == null || bookResponse.getBooks() == null || bookResponse.getBooks().size() == 0) {
+                            booksContract.displayEmptyState();
+                        }
+                        ArrayList<Book> books = bookResponse.getBooks();
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        booksContract.displayErrorState();
+                    }
+                }));
+    }
+
+    void unbind() {
+        compositeSubscription.unsubscribe();
+    }
+
+
+}
