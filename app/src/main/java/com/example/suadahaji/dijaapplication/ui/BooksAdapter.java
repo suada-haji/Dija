092c58ed73d99dc6ff9722a8c97f049c8704c565
Suada Haji <suada.haji@andela.com>
2017-03-21 22:46:41 +0300

    Create Adapter for listing books

---
+++ app/src/main/java/com/example/suadahaji/dijaapplication/ui/BooksAdapter.java
@@ -0,0 +1,71 @@
+package com.example.suadahaji.dijaapplication.ui;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.suadahaji.dijaapplication.R;
+import com.example.suadahaji.dijaapplication.models.Book;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by suadahaji
+ */
+
+public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BooksHolder>{
+
+    private ArrayList<Book> books;
+    int rowLayout;
+    Context context;
+
+    public BooksAdapter(ArrayList<Book> bookArrayList, int rowLayout, Context context) {
+        this.books = bookArrayList;
+        this.rowLayout = rowLayout;
+        this.context = context;
+    }
+
+    public class BooksHolder extends RecyclerView.ViewHolder {
+        TextView bookName;
+        TextView bookDescription;
+        TextView bookAuthor;
+        ImageView bookImage;
+        TextView bookPrice;
+
+        public BooksHolder(View itemView) {
+            super(itemView);
+            bookName = (TextView) itemView.findViewById(R.id.book_name);
+            bookDescription = (TextView) itemView.findViewById(R.id.book_description);
+            bookAuthor = (TextView) itemView.findViewById(R.id.book_author);
+            bookImage = (ImageView) itemView.findViewById(R.id.book_image);
+            bookPrice = (TextView) itemView.findViewById(R.id.book_price);
+        }
+    }
+    @Override
+    public BooksHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.activity_list_books, parent, false);
+        return new BooksHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(BooksAdapter.BooksHolder holder, int position) {
+        holder.bookName.setText(books.get(position).getBookName());
+        holder.bookDescription.setText(books.get(position).getBookDescription());
+        holder.bookAuthor.setText(books.get(position).getBookAuthor());
+        holder.bookPrice.setText(Double.toString(books.get(position).getBookPrice()));
+        Picasso.with(context).load(books.get(position).getBookImage()).into(holder.bookImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return books.size();
+    }
+
+
+}
--- app/src/main/java/com/example/suadahaji/dijaapplication/ui/SplashActivity.java
+++ app/src/main/java/com/example/suadahaji/dijaapplication/ui/SplashActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-import com.example.suadahaji.dijaapplication.ListBooksActivity;
-
 public class SplashActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 3000;
