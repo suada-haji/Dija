b7233fd341ed6ced3bfd9f2ae6b23e88f8b3ee20
Suada Haji <suada.haji@andela.com>
2017-03-31 16:10:54 +0300

    Modify Background for Cart Icon

--- app/src/main/java/com/example/suadahaji/dijaapplication/ui/BookDetailActivity.java
+++ app/src/main/java/com/example/suadahaji/dijaapplication/ui/BookDetailActivity.java
@@ -1,12 +1,15 @@
 package com.example.suadahaji.dijaapplication.ui;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.support.v7.widget.Toolbar;
+import android.support.v7.graphics.Palette;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,14 +17,13 @@ import com.example.suadahaji.dijaapplication.R;
 import com.example.suadahaji.dijaapplication.dagger.BooksApplication;
 import com.example.suadahaji.dijaapplication.models.Book;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class BookDetailActivity extends AppCompatActivity {
-
-    private static final String TAG = "BookDetailActivity";
 
     @BindView(R.id.book_detail_image)
     ImageView bookImage;
@@ -32,6 +34,9 @@ public class BookDetailActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+    @BindView(R.id.fab_cart)
+    FloatingActionButton fabCart;
 
     @BindView(R.id.book_detail_price)
     TextView bookPrice;
@@ -68,13 +73,40 @@ public class BookDetailActivity extends AppCompatActivity {
 
         Book book = getIntent().getParcelableExtra("currentBook");
 
-        Picasso.with(this).load(book.getBookImage()).into(bookImage);
+        Picasso.with(this).load(book.getBookImage()).into(new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+               assert  bookImage != null;
+                bookImage.setImageBitmap(bitmap);
+                Palette.from(bitmap)
+                        .generate(new Palette.PaletteAsyncListener() {
+                            @Override
+                            public void onGenerated(Palette palette) {
+                                Palette.Swatch textSwatch = palette.getVibrantSwatch();
+                                fabCart.setBackgroundTintList(ColorStateList.valueOf(textSwatch.getRgb()));
+                            }
+                        });
+            }
 
-        bookPrice.setText("$ " + Double.toString(book.getBookPrice()));
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        });
+
+
+        bookPrice.setText(getString(R.string.price_annotate) + Double.toString(book.getBookPrice()));
         bookDescription.setText(book.getBookDescription());
         bookName.setText(book.getBookName());
         bookDate.setText(book.getPublishedYear());
-        bookAuthor.setText(book.getBookAuthor());
+        bookAuthor.setText(getString(R.string.author_annotate) + book.getBookAuthor());
+
+        fabCart.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorPrimary)));
     }
 
 
