f0b9e297e0aa947e2bab4d977d3b891cd82147ba
Suada Haji <suada.haji@andela.com>
2017-03-31 18:22:36 +0300

    Modify Api Url

--- app/src/main/java/com/example/suadahaji/dijaapplication/api/ApiManager.java
+++ app/src/main/java/com/example/suadahaji/dijaapplication/api/ApiManager.java
@@ -10,7 +10,7 @@ import rx.Observable;
  */
 
 public interface ApiManager {
-    @GET("3b0561062271123653f7569fbb941e20/raw/210ee4e48272eb3531b64a4eb4aa211ec647314a/6420cfdb-0af0-11e7-a0ba-d3c0c8656989.json")
+    @GET("353a0b5fcabebf53adf0b0d44cc512b3/raw/ec675ad39fe4d37bfb8e6acbd36154f9123b3a8a/blob.json")
     Observable<BookResponse> getBooks();
 
 }
